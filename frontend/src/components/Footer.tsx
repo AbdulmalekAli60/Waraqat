90dc32432a7b5dbc632a26c27d427617c0a90ec8
AbdulmalekAli60 <ZxshxZ123@gmail.com>
2025-03-08 20:50:38 +0300

    added copy symbol

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="w-full py-2 mt-auto border-t-2 border-black">
       <p className="font-bold text-sm sm:text-base  text-center ">
-      @All Rights Reserved
+      &copy;All Rights Reserved
       </p>
     </footer>
   );
